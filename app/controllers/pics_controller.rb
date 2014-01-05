8ee8a4534e32c3369626c723f8fc5973eb355e96
monster <Toby@dhcp-168-105-83-22.wireless.manoa.hawaii.edu>
2014-01-04 17:28:01 -1000

    Hooked up our database to our form and make it all awesome.

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -5,6 +5,26 @@ class PicsController < ApplicationController
        @pics = Pic.all
 
     end
+    
+
+    def new 
+       
+       @pic = Pic.new
+
+    end
+
+    def create
+    	#Pic.create( :lesson =>1, ...)
+    	Pic.create( pic_params )
+    	redirect_to pics_path
+    end
+
+    private
+   
+    def pic_params
+    	params.require(:pic).permit(:lesson, :emotion, :learned)
+
+    end
 
 
 end
